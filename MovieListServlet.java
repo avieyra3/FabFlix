734cc81d55d5330bef39f854a74cfa5b988b2ef0
Alfonso <a.vieyra@hotmail.co.uk>
2023-04-09 20:49:19 -0700

    added movie list servlist to alfonso_branch

---
+++ MovieListServlet.java
@@ -0,0 +1,85 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "MovieListServlet", urlPatterns = "/api/movielist")
+public class MovieListServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        System.out.println("DOGET EXECUTING");
+        response.setContentType("application/json");
+
+        PrintWriter out = response.getWriter();
+
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("Connection established!\n");
+            Statement statement = connection.createStatement();
+            String query = "SELECT * \n" +
+                    "FROM movies JOIN ratings\n" +
+                    "WHERE movies.id = ratings.movieId\n" +
+                    "ORDER BY ratings.rating DESC\n" +
+                    "LIMIT 20;";
+            ResultSet result = statement.executeQuery(query);
+
+            JsonArray jsonArray = new JsonArray();
+
+            while (result.next()) {
+                String movie_id = result.getString("id");
+                String movie_title = result.getString("title");
+                String movie_year = result.getString("year");
+                String movie_director = result.getString("director");
+                String movie_rating = result.getString("rating");
+
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movie_id);
+                jsonObject.addProperty("movie_title", movie_title);
+                jsonObject.addProperty("movie_year", movie_year);
+                jsonObject.addProperty("movie_director", movie_director);
+                jsonObject.addProperty("movie_rating", movie_rating);
+
+                jsonArray.add(jsonObject);
+            }
+            result.close();
+            statement.close();
+
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+
+            out.write(jsonArray.toString());
+            response.setStatus(200);
+
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("ERROR:", e.getMessage());
+            out.write(jsonObject.toString());
+
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+
+}
