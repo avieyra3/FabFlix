5238d79bf512162d5d329487cac9c69299712d4d
Khoi Pham <phamkhoiedison@gmail.com>
2023-04-22 13:59:11 -0700

    Split index into separate parts, split index servlet into index and shopping cart, create new servlets for browsing

--- src/ShoppingCartServlet.java
+++ src/ShoppingCartServlet.java
@@ -11,11 +11,11 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * This IndexServlet is declared in the web annotation below,
- * which is mapped to the URL pattern /api/index.
+ * This ShoppingCartServlet is declared in the web annotation below,
+ * which is mapped to the URL pattern /api/cart.
  */
-@WebServlet(name = "IndexServlet", urlPatterns = "/api/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet(name = "ShoppingCartServlet", urlPatterns = "/api/cart")
+public class ShoppingCartServlet extends HttpServlet {
 
     /**
      * handles GET requests to store session information
@@ -40,6 +40,37 @@ public class IndexServlet extends HttpServlet {
         responseJsonObject.add("previousItems", previousItemsJsonArray);
 
         // write all the data into the jsonObject
+        response.getWriter().write(responseJsonObject.toString());
+    }
+
+    /**
+     * handles POST requests to add and show the item list information
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String item = request.getParameter("item");
+        System.out.println(item);
+        HttpSession session = request.getSession();
+
+        // get the previous items in a ArrayList
+        ArrayList<String> previousItems = (ArrayList<String>) session.getAttribute("previousItems");
+        if (previousItems == null) {
+            previousItems = new ArrayList<String>();
+            previousItems.add(item);
+            session.setAttribute("previousItems", previousItems);
+        } else {
+            // prevent corrupted states through sharing under multi-threads
+            // will only be executed by one thread at a time
+            synchronized (previousItems) {
+                previousItems.add(item);
+            }
+        }
+
+        JsonObject responseJsonObject = new JsonObject();
+
+        JsonArray previousItemsJsonArray = new JsonArray();
+        previousItems.forEach(previousItemsJsonArray::add);
+        responseJsonObject.add("previousItems", previousItemsJsonArray);
+
         response.getWriter().write(responseJsonObject.toString());
     }
 }
