8f62bb7ff1a6947aaa7a6ed001824f338f8396da
Khoi Pham <phamkhoiedison@gmail.com>
2023-04-23 03:29:35 -0700

    search by star name now works by splitting up the query

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -46,7 +46,7 @@ public class MovieListServlet extends HttpServlet {
 
             System.out.println(title + " " + year + " " + director + " " + star);
 
-            queryAmmend = "WHERE title LIKE '%" + title + "%' AND director LIKE '%" + director + "%'";
+            queryAmmend = " AND title LIKE '%" + title + "%' AND director LIKE '%" + director + "%' AND stars.name LIKE '%" + star + "%'";
             if (year != "") {
                 queryAmmend = queryAmmend + " AND year = '" + year + "' ";
             }
@@ -61,46 +61,72 @@ public class MovieListServlet extends HttpServlet {
         try (Connection connection = dataSource.getConnection()) {
             System.out.println("MovieList Connection established!\n");
             Statement statement = connection.createStatement();
-            String query = 
-            "SELECT m.id AS movie_id, m.title AS title, m.year AS year, m.director AS director, " +
-            "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT g.name SEPARATOR ', '), ', ', 3) AS genres, " +
-            "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT s.name), ',', 3) AS stars, " +
-            "SUBSTRING_INDEX(GROUP_CONCAT(DISTINCT s.id ORDER BY s.name), ',', 3) AS star_id," +
-            "r.rating AS rating " +
-            "FROM movies m " +
-            "JOIN genres_in_movies gm ON m.id = gm.movieId " +
-            "JOIN genres g ON gm.genreId = g.id " +
-            "JOIN stars_in_movies sm ON m.id = sm.moviesId " +
-            "JOIN stars s ON sm.starId = s.id " +
-            "JOIN ratings r ON m.id = r.movieId " +
-             queryAmmend +
-            "GROUP BY m.id, m.title, m.year, m.director, r.rating " +
-            "ORDER BY r.rating DESC " +
-            "LIMIT 20";
-            System.out.println(query);
+            String query = "SELECT DISTINCT movies.id, movies.title, movies.year, movies.director, rating\n" +
+                    "FROM movies JOIN ratings JOIN stars_in_movies JOIN stars JOIN genres_in_movies JOIN genres\n" +
+                    "WHERE movies.id = ratings.movieId AND movies.id = stars_in_movies.moviesId AND stars_in_movies.starId = stars.id \n" +
+                    "AND movies.id = genres_in_movies.movieId AND genres_in_movies.genreId = genres.id\n" +
+                    queryAmmend +
+                    "ORDER BY ratings.rating DESC\n" +
+                    "LIMIT 20;";
             ResultSet result = statement.executeQuery(query);
 
             JsonArray jsonArray = new JsonArray();
 
             while (result.next()) {
-                String movie_id = result.getString("movie_id");
+                String movie_id = result.getString("id");
                 String movie_title = result.getString("title");
                 String movie_year = result.getString("year");
                 String movie_director = result.getString("director");
-                String movie_genres = result.getString("genres");
-                String movie_stars = result.getString("stars");
-                String star_id = result.getString("star_id");
                 String movie_rating = result.getString("rating");
+                String movie_genres = "";
+                String movie_stars = "";
+                String movie_star_IDs = "";
+
+                Statement statementGenres = connection.createStatement();
+                String queryGenres = "SELECT genres.name\n" +
+                        "FROM movies JOIN genres_in_movies JOIN genres\n" +
+                        "WHERE movies.id = genres_in_movies.movieId AND genres_in_movies.genreId = genres.id " +
+                        "AND movies.id = '" + movie_id + "'\n" +
+                        "LIMIT 3;";
+                ResultSet resultGenres = statementGenres.executeQuery(queryGenres);
+                while (resultGenres.next()) {
+                    movie_genres += resultGenres.getString("name") + ", ";
+                }
+
+                Statement statementStars = connection.createStatement();
+                String queryStars = "SELECT stars.name\n" +
+                        "FROM movies JOIN stars_in_movies JOIN stars\n" +
+                        "WHERE movies.id = stars_in_movies.moviesId AND stars_in_movies.starId = stars.id " +
+                        "AND movies.id = '" + movie_id + "'\n" +
+                        "LIMIT 3;";
+                ResultSet resultStars = statementStars.executeQuery(queryStars);
+                while (resultStars.next()) {
+                    movie_stars += resultStars.getString("name") + ", ";
+                }
+
+                Statement statementStarIDs = connection.createStatement();
+                String queryStarIDs = "SELECT stars.id\n" +
+                        "FROM movies JOIN stars_in_movies JOIN stars\n" +
+                        "WHERE movies.id = stars_in_movies.moviesId AND stars_in_movies.starId = stars.id " +
+                        "AND movies.id = '" + movie_id + "'\n" +
+                        "LIMIT 3;";
+                ResultSet resultStarIDs = statementStarIDs.executeQuery(queryStarIDs);
+                while (resultStarIDs.next()) {
+                    movie_star_IDs += resultStarIDs.getString("id") + ", ";
+                }
+
+                System.out.println(movie_id + " " + movie_title + " " + movie_year + " " + movie_director
+                        + " " + movie_rating + " " + movie_genres + " " + movie_stars);
 
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("movie_id", movie_id);
                 jsonObject.addProperty("movie_title", movie_title);
                 jsonObject.addProperty("movie_year", movie_year);
                 jsonObject.addProperty("movie_director", movie_director);
+                jsonObject.addProperty("movie_rating", movie_rating);
                 jsonObject.addProperty("movie_genres", movie_genres);
                 jsonObject.addProperty("movie_stars", movie_stars);
-                jsonObject.addProperty("star_id", star_id);
-                jsonObject.addProperty("movie_rating", movie_rating);
+                jsonObject.addProperty("star_id", movie_star_IDs);
 
                 jsonArray.add(jsonObject);
             }
