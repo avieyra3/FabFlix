e5a9cb55cf59febc21ad68c50847eda7b4620523
Alfonso <a.vieyra@hotmail.co.uk>
2023-06-12 15:01:02 -0700

    changed log processing to take any file.

--- log_processing.py
+++ log_processing.py
@@ -1,22 +1,32 @@
-with open("timelog", "r") as f:
+import sys
 
-    average_tj: int = 0
-    average_ts: int = 0
+def log_processing(text):
 
-    # read each line and parse, the format is constant so we can assume
-    # that the below loop will always parse correctly
-    # example line: TJ 123456 TS 987654
-    #               0    1     2   3
-    lines = f.readlines()
-    # get sum for tj & ts
-    for line in lines:
-        line_list = line.split()
-        average_tj += int(line_list[1])
-        average_ts += int(line_list[3])
+    with open(text, "r") as f:
 
-    # calculate average
-    average_tj = average_tj / len(lines)
-    average_ts = average_ts / len(lines)
-    print("average tj = " + str(average_tj))
-    print("average ts = " + str(average_ts))
-    f.close()
+        average_tj: int = 0
+        average_ts: int = 0
+
+        # read each line and parse, the format is constant so we can assume
+        # that the below loop will always parse correctly
+        # example line: TJ 123456 TS 987654
+        #               0    1     2   3
+        lines = f.readlines()
+        #get sum for tj & ts
+        for line in lines:
+            line_list = line.split()
+            average_tj += int(line_list[1])
+            average_ts += int(line_list[3])
+
+        # calculate average
+        average_tj = average_tj / len(lines)
+        average_ts = average_ts / len(lines)
+        print("average tj = " + str(average_tj))
+        print("average ts = " + str(average_ts))
+        f.close()
+
+def main():
+    log_processing(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
